2fa64c8e6ff8ce8ffb4f60c4c6570256a79fc74d
徐劭伟 <wei@xushaoweideMac-mini.local>
2018-05-08 01:38:10 +0800

    会议日程 直播间

--- src/views/Date.vue
+++ src/views/Date.vue
@@ -5,27 +5,27 @@
         <div :class="`nav-btn ${activeNav === 0 ?'active' :''}`" @click="activeNav = 0">
           <img src="../../static/icon/date-icon.png" alt="">
           <span class="week">星期日</span>
-          <span class="date">6月10日</span>
+          <span class="date">6月9日</span>
         </div>
         <div :class="`nav-btn ${activeNav === 1 ?'active' :''}`"  @click="activeNav = 1">
           <img src="../../static/icon/date-icon.png" alt="">
           <span class="week">星期一</span>
-          <span class="date">6月11日</span>
+          <span class="date">6月10日</span>
         </div>
         <div :class="`nav-btn ${activeNav === 2 ?'active' :''}`"  @click="activeNav = 2">
           <img src="../../static/icon/date-icon.png" alt="">
           <span class="week">星期二</span>
-          <span class="date">6月12日</span>
+          <span class="date">6月11日</span>
         </div>
       </div>
     </div>
     
     <ul class="detail">
-      <li v-for="(item,index) in detail" :key="index">
+      <li v-for="(item,index) in detail[activeNav]" :key="index">
         <p class="time">时间 / <span :class="`${item.timeColor}`">{{item.time}}</span></p>
         <div class="info">
           <p class="title">{{item.title}}</p>
-          <p class="text">{{item.text}}</p>
+          <p class="text" v-html="item.text"></p>
         </div>
       </li>
     </ul>
@@ -37,54 +37,72 @@ export default {
     return{
       activeNav : 0,
       detail:[
-        {
-          time:'上午 10：00',
-          timeColor:'blue',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'下午 14：00',
-          timeColor:'orange',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'上午 10：00',
-          timeColor:'blue',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'下午 14：00',
-          timeColor:'orange',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'上午 10：00',
-          timeColor:'blue',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'下午 14：00',
-          timeColor:'orange',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'上午 10：00',
-          timeColor:'blue',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
-        {
-          time:'下午 14：00',
-          timeColor:'orange',
-          title:'2018中国（石家庄)“互联网+旅游”特色小镇峰会',
-          text:'14：00 - 16：00   石家庄市鹿泉区国山宾馆'
-        },
+        [
+          {
+            time:'14：00-16：00',
+            timeColor:'orange',
+            title:'2018中国（石家庄）“互联网+旅游”特色小镇峰会',
+            text:'石家庄市鹿泉区国山宾馆'
+          },
+          {
+            time:'19：50',
+            timeColor:'orange',
+            title:'第二届石家庄市旅游产业发展大会开幕式',
+            text:'石家庄市鹿泉区西部长青景区水上乐园'
+          },
+          {
+            time:'开幕式后',
+            timeColor:'orange',
+            title:'大型实景演出',
+            text:'石家庄市鹿泉区西部长青景区水上乐园'
+          },
+          {
+            time:'全天',
+            timeColor:'blue',
+            title:'第五届石家庄旅游交易会',
+            text:'石家庄市鹿泉智慧城市广场'
+          }
+        ],
+        [
+          {
+            time:'08：00-18：00',
+            timeColor:'blue',
+            title:'旅游项目线路观摩',
+            text:'观摩路线：<br>（1）鹿泉智慧城市广场；<br>（2）多彩小镇花海；<br>（3）卧龙山大年初一风情小镇；<br>（4）岸下•石窑小镇；<br>（5）谷家峪美丽乡村旅游度假村；<br>（6）土门关•驿道小镇；<br>（7）抱犊寨景区；<br>（8）西部长青旅游度假区；<br>（9）龙泉古镇；<br>（10）龙泉湖湿地；<br>（11）封龙农谷农科小镇；<br>（12）杰明房车营地；<br>（13）北国奥特莱斯；<br>（14）君乐宝奶业小镇；<br>（15）食草堂艺术小镇；<br>（16）十里花廊；<br>（17）稻香村；<br>（18）长城影视；<br>（19）紫藤西山庄园。'
+          },
+          {
+            time:'全天（10：00 开幕式）',
+            timeColor:'blue',
+            title:'第五届石家庄旅游交易会',
+            text:'石家庄市鹿泉智慧城市广场'
+          }
+        ],
+        [
+          {
+            time:'8：50-9：50',
+            timeColor:'blue',
+            title:'《全民导游》颁奖典礼。',
+            text:'全市全域旅游推进会地点。'
+          },
+          {
+            time:'10：00—11：00',
+            timeColor:'blue',
+            title:'石家庄市创建全域旅游示范市工作推进会',
+            text:'石家庄市鹿泉智慧城市广场'
+          },
+          {
+            time:'10：00—11：00',
+            timeColor:'blue',
+            title:'石家庄市创建全域旅游示范市工作推进会',
+            text:'石家庄市鹿泉智慧城市广场'
+          },
+          {
+            time:'全天',
+            timeColor:'blue',
+            title:'第五届石家庄旅游交易会',
+            text:'石家庄市鹿泉智慧城市广场'
+          }
+        ]
       ]
     }
   }
@@ -97,7 +115,8 @@ export default {
   position: absolute;
   width: 100%;
   min-height: 100%;
-  background-color: #a7f0f1;
+  // background-color: #a7f0f1;
+  background: -webkit-linear-gradient(#a7f0f1, #ffffff); /* Safari 5.1 - 6.0 */
   
 
   .nav-fixed{
@@ -170,7 +189,7 @@ export default {
     box-sizing: border-box;
     padding: 10px;
     margin-top: calc(20px + 14vw);
-    background: -webkit-linear-gradient(#a7f0f1, #ffffff); /* Safari 5.1 - 6.0 */
+    // background: -webkit-linear-gradient(#a7f0f1, #ffffff); /* Safari 5.1 - 6.0 */
 
     li{
       position: relative;
@@ -208,6 +227,7 @@ export default {
           color: #3d3c3c;
           font-size: 14px;
           margin-top: 15px;
+          line-height: 1.5;
         }
       }
     }
--- src/views/Living.vue
+++ src/views/Living.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="living">
-    <img class="head" src="../assets/living-head.jpg" @click="e">
+    <img class="head" src="../assets/living-head1.jpg" @click="e">
     <div class="msg">
       <div class="linear-gradient-bg"></div>
       <ul class="text">
         <li class="item" v-for="(item,index) in livingMsg" :key="index">
-          <p class="item-time">{{item.time}}</p>
+          <p class="item-time">{{`2018-6-${new Date(item.time).getDate()} ${new Date(item.time).getHours()}:${new Date(item.time).getMinutes() < 10 ? '0'+new Date(item.time).getMinutes() : new Date(item.time).getMinutes()}`}}</p>
           <img class="item-img" v-if="item.img" :src="item.img" alt="">
           <p class="item-text" v-if="item.text">{{item.text}}</p>
         </li>
@@ -17,29 +17,30 @@
 export default {
   data(){
     return{
-      livingMsg:[
-        {
-          time:'2018-06-10 14:00',
-          img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525600667966&di=cc5b7a40c04360c0cd89aaac8754ff59&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dpixel_huitu%252C0%252C0%252C294%252C40%2Fsign%3D0d3aeeb8c01349546a13e0243f36f734%2Fc83d70cf3bc79f3dab39b209b1a1cd11728b292a.jpg',
-          text:'河北省省常委石家庄市委书记宣布首届,河北省省常委石家庄市委书记宣布首届'
-        },
-        {
-          time:'2018-06-10 14:00',
-          img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525600667966&di=cc5b7a40c04360c0cd89aaac8754ff59&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimage%2Fc0%253Dpixel_huitu%252C0%252C0%252C294%252C40%2Fsign%3D0d3aeeb8c01349546a13e0243f36f734%2Fc83d70cf3bc79f3dab39b209b1a1cd11728b292a.jpg',
-          text:''
-        },
-        {
-          time:'2018-06-10 14:00',
-          img:'',
-          text:'河北省省常委石家庄市委书记宣布首届,河北省省常委石家庄市委书记宣布首届'
-        },
-      ]
+      livingMsg:[]
     }
   },
   methods:{
     e(e){
       e.preventDefault();
-    }
+    },
+    getMsg(){
+      this.axios({
+        method: 'get',
+        url: '/livingmsg',
+        params:{
+          meeting: 5
+        },
+        baseURL: this.host.baseUrl
+        //baseURL:'http://10.168.34.43:3008/api'
+      }).then((res)=>{
+        console.log(res)
+        this.livingMsg = res.data.data;
+      })
+    },
+  },
+  mounted(){
+    this.getMsg();
   }
 }
 </script>
@@ -49,18 +50,15 @@ export default {
   width: 100%;
   min-height: 100%;
   background-color: #fff;
-
   .head{
     position: relative;
     width: 100%;
     vertical-align: top;
   }
-
   .msg{
     // b7ebf8
     position: relative;
     padding-bottom: 30px;
-
     
     .linear-gradient-bg{
       position: absolute;
@@ -68,7 +66,6 @@ export default {
       height: 300px;
       background: -webkit-linear-gradient(#b7ebf8, #ffffff); /* Safari 5.1 - 6.0 */
     }
-
     .text{
       position: relative;
       width: 80vw;
@@ -77,7 +74,6 @@ export default {
       padding: 20px;
       background-color: #99ecef;
       text-align: center;
-
       .item{
         display: inline-block;
         width: 100%;
@@ -87,18 +83,15 @@ export default {
         box-sizing: border-box;
         padding: 10px;
         background-color: #ffffff;
-
         .item-time{
           font-size: 18px;
           text-align: left;
           color: #4671d8;
         }
-
         .item-img{
           width: 100%;
           margin-top: 5px;
         }
-
         .item-text{
           text-align: left;
           font-size: 14px;
@@ -110,4 +103,3 @@ export default {
   }
 }
 </style>
-
