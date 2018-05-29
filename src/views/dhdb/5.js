7d55f92b7fc280a80065b356b073b44402d9652b
徐劭伟 <wei@xushaoweideMac-mini.local>
2018-05-29 23:00:26 +0800

    文字修改

---
+++ src/views/dhdb/5.js
@@ -0,0 +1,146 @@
+let data = [
+  {
+    type:'text',
+    data:'每当穿过拥挤人群，仰望城市的高楼林立，重复着平淡琐碎的工作，是否有那么一个瞬间，您想逃离周遭的喧嚣，退隐郊野的静谧去寻找片刻的闲适……不妨试试走进距离城区最近的都市牧场，用15公里让您与繁华都市暂时隔离——君乐宝优致牧场，带您感受一次别开生面的绝美体验。'
+  },
+  {
+    type:'text',
+    data:'与其说是牧场，它更像是个充满欧式风情的童话小镇，路边随风清扬的格桑花、翠绿欲滴的片片牧草都在呼唤着来自四面八方的游客。'
+  },
+
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453445226016045893.png'
+  },
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453445226016044869.png'
+  },
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453445226126085009.png'
+  },
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453445226720062715.png'
+  },
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453445226782003004.png'
+  },
+
+  {
+    type:'text',
+    data:'【坐落在山间的牧场，在冬日也别有一番情趣】'
+  },
+  {
+    type:'text',
+    data:'一进牧场大门我们的小牛宝宝就已经在恭候您的大驾啦！您可以亲身体验与牛宝宝玩耍的乐趣！'
+  },
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453445931675050624.png'
+  },
+  {
+    type:'text',
+    data:'沿着观光大道继续向前，就来到了国内最大的奶牛文化科普馆，奶牛家族的前世今生就都在这里啦!'
+  },
+  {
+    type:'text',
+    data:'奶牛有公牛吗？！'
+  },
+  {
+    type:'text',
+    data:'奶牛有犄角吗？'
+  },
+  {
+    type:'text',
+    data:'奶牛会放屁吗？'
+  },
+  {
+    type:'text',
+    data:'优致牧场的奶牛过着怎样的生活呢？'
+  },
+  {
+    type:'text',
+    data:'.........'
+  },
+  {
+    type:'text',
+    data:'各种问题在这里通通可以得到解答，不仅为您提供视觉的盛宴，更带您走进知识的海洋！'
+  },
+
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453446007926000985.png'
+  },
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453446008144073276.png'
+  },
+  
+  {
+    type:'text',
+    data:'走进宽敞、明亮、时尚的挤奶大厅，透过落地窗，您可以清晰的看到奶牛们如何挤奶的。现代化转盘式挤奶机载着奶牛们在转动的同时即可自动挤奶，这可是全球领先的挤奶技术哦！！'
+  },
+
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453446094348053971.png'
+  },
+
+  {
+    type:'text',
+    data:'如果您还不尽兴，别急，亚洲最大的酸奶工厂与国内首家酸奶文化馆同样期待您的光临！'
+  },
+
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453446790022060424.png'
+  },
+
+  {
+    type:'text',
+    data:'最最重要的是在这里您还可以看到目前具有国际领先技术装备水平的酸奶生产车间，老酸奶，开啡尔、每日活菌、君畅等等明星产品统统都在这里诞生的。'
+  },
+
+  {
+    type:'img',
+    data:'http://www.pingshanlvyou.com/pub1/ueditor/upload/image/20160122/1453446841960035240.png'
+  },
+
+  
+
+
+  {
+    type:'info',
+    title:'交通方式',
+    data:[
+      '【优致牧场】：',
+      '自驾路线1：市内沿槐安西路过3环西行至京赞公路（即上庄大街）南行5公里至金凤路西行3公里即可到达景区。',
+      '自驾路线2：石铜路青银高速交叉口西行至第一个红绿灯路口右转至繁荣大街北行2公里左转至金凤路西行3公里即可到达景区。',
+      '公交路线：乘坐331路在君乐宝牧场下车即可到达景区。',
+      '【酸奶工厂】',
+      '自驾路线：沿石铜路西行至青银高速交叉口，西行200米路南即可到达景区。',
+      '公交路线：乘坐331、311、游2路在河北天森（君乐宝）下车即可到达景区。',
+    ]
+  },
+
+  {
+    type:'info',
+    title:'参观时间',
+    data:[
+      '【优致牧场】：',
+      '每周一至周日  8:30——17:00  ，随到随入。',
+      '注：奶牛每天要挤3次奶，挤奶时间一般是上午6:30—10:30；下午14:00—18：00；晚上22:00—02:00，所以建议您参观时间为8:30——11:00    14:00——16:00',
+      '【酸奶工厂】',
+      '入场时间  10:30     15:00',
+      '五一、十一会根据游客量增加入场批次，详情请咨询电话：0311——67362691',
+    ]
+  }
+]
+
+module.exports = {
+  name:'君乐宝奶业小镇',
+  data
+}
