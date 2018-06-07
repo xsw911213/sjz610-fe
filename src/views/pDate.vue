e1922f7496d7dcfa7f36833bf2027bf09cfffaa4
xushaowei10077 <xushaowei10077@autohome.com.cn>
2018-06-07 16:53:58 +0800

    路由线路

--- src/views/pDate.vue
+++ src/views/pDate.vue
@@ -29,7 +29,13 @@
         </div>
       </li>
     </ul>
-    线路{{linetype}}
+    <div class="line" v-if="activeNav">
+      <p v-for="(item,index) in lines[linetype].split('→')" :key="index">
+        {{item}}
+      </p>
+      <!-- {{lines[linetype]}} -->
+    </div>
+    
   </div>
 </template>
 <script>
@@ -39,6 +45,12 @@ export default {
       activeNav : 0,
       detail:[[],[],[]],
       linetype:'a',
+      lines:{
+        a:'西部长青璞祯酒店→西部长青德明古镇→新杏苑路→山前大道→动物园（路过）→抱犊寨→山前大道→鹿泉智慧城市中心→山前大道→西北外环→大年初一风情小镇→西北外环→环抱北路→花田原乡小镇（路过）→岸下石窑小镇（路过）→环抱西路→谷家峪美丽乡村（路过）→环抱南路→土门关驿道小镇→环抱南路→山前大道→河北国山宾馆午餐→北斗路→花西街→望佛路→十里花廊（路过）→石柏大街→京赞线→食草堂艺术园区→京赞线→金凤路→君乐宝奶业小镇→金凤路→青银辅路→石铜路→碧水街→青龙山大道→北国奥特莱斯·水世界→碧水街→旅游路→杰明房车木屋营地→旅游路→羊角庄路→衡井线→山前大道→紫藤西山庄园（路过）→中以农科小镇→山前大道→南二环西延→龙泉寺（路过）→西部长青璞祯酒店',
+        b:'国源朗怡酒店→京赞线→槐安西路→龙泉古镇（路过）→山前大道→龙泉湖西门（路过）→河北长城影视文创园（路过）→南二环西延→龙泉湖南门（路过）→京赞线→食草堂艺术园区→京赞线→金凤路→君乐宝奶业小镇→金凤路→青银辅路→石铜路→碧水街→青龙山大道→北国奥特莱斯·水世界→碧水街→旅游路→杰明房车木屋营地→山前大道→南二环西延→龙泉寺（路过）→西部长青璞祯酒店午餐→西部长青柳仙谷停车场→西部长青德明古镇→新杏苑路→山前大道→动物园（路过）→抱犊寨→山前大道→鹿泉智慧城市中心→山前大道→西北外环→大年初一风情小镇→西北外环→环抱北路→花田原乡小镇（路过）→岸下石窑小镇（路过）→环抱西路→谷家峪美丽乡村（路过）→环抱南路→土门关驿道小镇→环抱南路→山前大道→北斗路→花西街→望佛路→十里花廊（路过）→石柏大街→京赞线→国源朗怡酒店',
+        c:'国源朗怡酒店→京赞线→食草堂艺术园区（路过）→金凤路→君乐宝奶业小镇→金凤路→青银辅路→石铜路→碧水街→青龙山大道→北国奥特莱斯·水世界→碧水街→旅游路→杰明房车木屋营地→旅游路→羊角庄路→衡井线→山前大道→河北长城影视文创园（路过）→龙泉湖西门（路过）→龙泉古镇（路过）→众诚假日酒店午餐→山前大道→抱犊寨→山前大道→鹿泉智慧城市中心→山前大道→环抱北路→花田原乡小镇→环抱北路→岸下石窑小镇（路过）→环抱西路→谷家峪美丽乡村（路过）→环抱南路→→土门关驿道小镇→环抱南路→山前大道→动物园（路过）→新杏苑路→西部长青柳仙谷停车场→西部长青德明古镇→南二环西延→龙泉寺（路过）→龙泉湖南门（路过）→京赞线→食草堂艺术园区→京赞线→国源朗怡酒店',
+        d:'众诚假日酒店→山前大道→抱犊寨→山前大道→鹿泉智慧城市中心→山前大道→环抱北路→花田原乡小镇（路过）→岸下石窑小镇（路过）→环抱西路→谷家峪美丽乡村（路过）→环抱南路→土门关驿道小镇→环抱南路→山前大道→动物园（路过）→新杏苑路→西部长青德明古镇→南二环西延→龙泉寺（路过）→龙泉湖南门（路过）→京赞线→国源朗怡午餐→京赞线→食草堂艺术园区→京赞线→金凤路→君乐宝奶业小镇→金凤路→青银辅路→石铜路→碧水街→青龙山大道→北国奥特莱斯·水世界→碧水街→旅游路→杰明房车木屋营地→旅游路→羊角庄路→衡井线→山前大道→河北长城影视文创园（路过）→龙泉湖西门（路过）→龙泉古镇→山前大道→众诚假日酒店'
+      }
     }
   },
   mounted(){
@@ -46,8 +58,8 @@ export default {
     console.log(remark)
     let arr1 = remark.split(';');
     console.log(arr1)
-    this.linetype = arr1[1].split('#')[1]
-    arr1[1] = arr1[1].split('#')[0]
+    this.linetype = arr1[1].split('#')[1] || 'a';
+    arr1[1] = arr1[1].split('#')[0];
     arr1.forEach((element,index) => {
       if(element){
         let arr = element.split(',');
@@ -77,7 +89,7 @@ export default {
   position: absolute;
   width: 100%;
   min-height: 100%;
-  padding-top: 45px;
+  // padding-top: 45px;
 
   // background-color: #a7f0f1;
   background: -webkit-linear-gradient(#a7f0f1, #ffffff); /* Safari 5.1 - 6.0 */
@@ -96,7 +108,7 @@ export default {
     .nav{
       display: flex;
       padding: 2vw;
-      margin-top: 45px;
+      // margin-top: 45px;
       justify-content: space-around;
       // background-color: rgba(#a7f0f1,.9);
       //background-color: transparent;
@@ -198,6 +210,25 @@ export default {
           line-height: 1.5;
         }
       }
+    }
+  }
+
+  .line{
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    position: relative;
+    padding: 15px 10px;
+    background-color: #fff;
+    margin: 15px 10px;
+    border-radius: 5px;
+    -webkit-box-shadow: 2px 3px 5px #888888;
+    box-shadow: 2px 3px 5px #888888;
+
+    p{
+      color: #3d3c3c;
+      font-size: 15px;
+      margin-top: 5px;
+      line-height: 1.3;
     }
   }
 }
